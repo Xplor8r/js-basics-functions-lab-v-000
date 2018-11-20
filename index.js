42aeea8e7b7e444ec666f72f3715c0ca7bc157cb
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-20 04:37:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(location) {
   let result;
-    location > 42 ? result = location - 42: result = location - 42;
+    location > 42 ? result = location - 42: result =  42 - location;
   return result;
 }
 
